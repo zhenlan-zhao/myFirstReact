30030d3e3d3d256c48da3b1ee6f33333f7ef348d
zdwzzlzdw <13102187679@163.com>
2020-03-15 20:33:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,9 +18,38 @@ function App() {
         >
           Learn React
         </a>
+        <Clock/>
       </header>
     </div>
   );
+}
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={date:new Date()};
+  }
+  componentDidMount(){
+    this.timerID=setInterval(
+      ()=>this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+  tick(){
+    this.setState({
+      date:new Date()
+    });
+  }
+  render(){
+    return(
+      <div>
+        <h1>Hello,zhaozhenlan</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
